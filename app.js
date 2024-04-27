0ca732a1540ee5fcd1cc044e731f484b15b37c01
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-04-27 03:47:27 -0700

    complete

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 let valueReciever = (val)=>{
 document.querySelector("#display").value += val;
 }
+
+
+let calculation = ()=>{
+    let result = eval(document.getElementById('display').value);
+    document.getElementById("display").value=result;
+
+}
+
+
+let clearFun = ()=>{
+    document.getElementById("display").value = "";
+}
